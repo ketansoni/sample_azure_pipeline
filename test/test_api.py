2839e45900e2839e86d50f01a80a79a6a66b0ffb
ketansoni <ketansony@gmail.com>
2019-12-27 16:44:52 -0500

    added timeout

--- test/test_api.py
+++ test/test_api.py
@@ -5,12 +5,13 @@ import json
 import polling
 import pytest
 
-personal_access_token = 'xxx'
+personal_access_token = 'mmqswhjnrvvck5r5a7lacsyt4juykd7gb5e4hwgz2mhvvzl4kmva'
 organization_url = "https://dev.azure.com/ketansony/6d52d6ae-d077-44f3-8024-97cadee02e1b"
 headers = {
-	    'authorization': "Basic xxx",
+	    'authorization': "Basic a2V0YW5zb255Om1tcXN3aGpucnZ2Y2s1cjVhN2xhY3N5dDRqdXlrZDdnYjVlNGh3Z3oybWh2dnpsNGttdmE=",
 	    'content-type': "application/json"
 	    }
+timeout=60	    
 
 def trigger_pipeline():
 	trigger_pipeline_querystring = {"ignoreWarnings":"false","api-version":"5.0"}
@@ -29,7 +30,7 @@ def wait_for_pipeline_status_to_be_completed(build_id):
 	    lambda: json.loads(requests.request("GET", pipeline_status_url, headers=headers).text).get('status'),
 	    check_success=is_correct_response,
 	    step=1,
-	    timeout=60)
+	    timeout=timeout)
 
 def get_pipeline_result(build_id):
 	return requests.request("GET", organization_url+"/_apis/build/Builds/"+str(build_id), headers=headers)
