efd150a9adb6e3bf367864d33423404eca668941
ketansoni <ketansony@gmail.com>
2019-12-26 21:14:35 -0500

    azure api status

--- test/api.py
+++ test/api.py
@@ -2,5 +2,19 @@ import pprint
 import requests
 import json
 
-r = requests.get('https://dev.azure.com/ketansony/6d52d6ae-d077-44f3-8024-97cadee02e1b/_apis/build/Builds/13', auth=('ketansony', personal_access_token))
-print(json.loads(r.text).get('result'))
+
+#Trigger Build Pipeline
+organization_url = "https://dev.azure.com/ketansony/6d52d6ae-d077-44f3-8024-97cadee02e1b"
+trigger_pipeline_querystring = {"ignoreWarnings":"false","api-version":"5.0"}
+trigger_pipeline_payload = "{\"definition\":{\"id\":1}}"
+headers = {
+    'content-type': "application/json"
+    }
+
+trigger_pipeline_response = requests.request("POST", organization_url+"/_apis/build/builds", data=trigger_pipeline_payload, headers=headers, params=trigger_pipeline_querystring)
+build_id = json.loads(trigger_pipeline_response.text).get('id')
+print(build_id)
+
+#Get Build Run Result
+pipeline_result_response = requests.request("GET", organization_url+"/_apis/build/Builds/"+str(build_id), headers=headers)
+print(json.loads(pipeline_result_response.text).get('status'))
