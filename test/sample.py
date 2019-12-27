4b2d0f4e539b5d4c8c6650247be1577342b0accb
ketansoni <ketansony@gmail.com>
2019-12-27 17:12:18 -0500

    assert files

--- test/sample.py
+++ test/sample.py
@@ -1,37 +1,34 @@
-# #!/usr/bin/env python3
-# import os
+import os
 
-# def getListOfFiles(dirName):
-#     listOfFile = os.listdir(dirName)
-#     allFiles = list()
-#     for entry in listOfFile:
-#         if os.path.isdir(fullPath):
-#             allFiles = allFiles + getListOfFiles(fullPath)
-#         else:
-#             allFiles.append(fullPath)
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
                 
-#     return allFiles        
+    return allFiles        
  
+def expected_list_of_files():
+	return ['./sample.py', 
+	'./test_api.py', 
+	'./.pytest_cache/CACHEDIR.TAG', 
+	'./.pytest_cache/README.md', 
+	'./.pytest_cache/.gitignore', 
+	'./.pytest_cache/v/cache/nodeids', 
+	'./.pytest_cache/v/cache/lastfailed', 
+	'./.pytest_cache/v/cache/stepwise', 
+	'./__pycache__/sample.cpython-37-pytest-5.3.2.pyc']
+
+def test_scaffolded_directories_and_files():
+    dirName = './';
  
-# def test_scaffolded_directories_and_files():
-#     dirName = './';
-
-#     expectedList = ["./requirements.txt",
-#         "./sample.py",
-#         "./.pytest_cache/CACHEDIR.TAG",
-#         "./.pytest_cache/README.md",
-#         "./.pytest_cache/.gitignore",
-#         "./.pytest_cache/v/cache/nodeids",
-#         "./.pytest_cache/v/cache/lastfailed",
-#         "./.pytest_cache/v/cache/stepwise",
-#         "./__pycache__/sample.cpython-37-pytest-5.3.2.pyc"]
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
     
-#     listOfFiles = list()
-#     for (dirpath, dirnames, filenames) in os.walk(dirName):
-#         listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+    print(listOfFiles)
+    assert listOfFiles == expected_list_of_files()    
     
-#     assert listOfFiles == expectedList    
-#     
-
-def test_sample():
-    assert True
